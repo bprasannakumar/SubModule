33c61a72b0b72caa9594fea5b7cd11f2a327b503
Prasanna Kumar <prasanna@atisfy.com>
2021-08-24 20:51:57 +0530

    added mainModule as submodule

--- app/submod_cron.py
+++ app/submod_cron.py
@@ -1,5 +1,9 @@
+from MainModule.app.main_cron import execute_main_cron
+
+
 def execute_submod_cron():
     print("execute_submod_cron from submod_cron.py")
+    execute_main_cron()
 
 
 if __name__ == "__main__":
