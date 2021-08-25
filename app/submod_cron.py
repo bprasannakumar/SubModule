98011a471782c0ad29ea1ed3eed7019ea986f133
Prasanna Kumar <prasanna@atisfy.com>
2021-08-25 18:14:31 +0530

    submod commit

--- app/submod_cron.py
+++ app/submod_cron.py
@@ -1,9 +1,24 @@
-from MainModule.app.main_cron import execute_main_cron
+#!/usr/bin/env
+
+import sys
+from importlib import import_module
+
+#sys.path.add('D:/GMG/submodule/SubModule/submod')
+sys.path.insert(1, 'D:\\GMG\\submodule\\SubModule\\submod')
+import os
+
+file_name = 'test_cron.py'
+path = os.path.abspath(file_name)
+
+sys.path.insert(0, path)
+
+from submod.test_cron import test_func
 
 
 def execute_submod_cron():
     print("execute_submod_cron from submod_cron.py")
-    execute_main_cron()
+    #print(sys.path)
+    print("path: ", path)
 
 
 if __name__ == "__main__":
---
+++ submod/test_cron.py
@@ -0,0 +1,10 @@
+import sys
+
+
+def test_func():
+    print(sys.path)
+    #execute_submod_cron()
+
+
+if __name__ == "__main__":
+    test_func()
